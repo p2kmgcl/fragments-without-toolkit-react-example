3e8b7006194d1ebab4713aba155ef4b9e76d61cf
Pablo Molina <contact@pablomolina.me>
2023-11-10 09:56:44 +0100

    init

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This could be also done with npm scripts and/or other npm tools
+# to be more flexible and robust.
+
+rm -rf build
+mkdir -p build/my-collection/my-fragment
+
+cp my-collection/collection.json build/my-collection
+
+cp my-collection/my-fragment/fragment.json build/my-collection/my-fragment
+cp my-collection/my-fragment/index.html build/my-collection/my-fragment
+cp my-collection/my-fragment/index.css build/my-collection/my-fragment
+
+npx esbuild my-collection/my-fragment/index.jsx --outfile=build/my-collection/my-fragment/index.js
+
+cd build && zip -qr my-collection.zip my-collection
+
+echo "build/my-collection.zip created"
---
+++ my-collection/my-fragment/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function MyFragment(props) {
+  console.log('MyFragment props', props);
+  return <div>Hey there!</div>;
+}
