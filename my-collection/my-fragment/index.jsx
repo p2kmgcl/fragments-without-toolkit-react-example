f35b55b3682d598f72f86fb6711b2b3fac4d684c
Pablo Molina <contact@pablomolina.me>
2023-11-15 10:41:49 +0100

    update

--- my-collection/my-fragment/index.jsx
+++ my-collection/my-fragment/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import { Counter } from "./Counter";
 
-export default function MyFragment(props) {
+function MyFragment(props) {
   console.log('MyFragment props', props);
   return <Counter />;
 }
+
+ReactDOM.unmountComponentAtNode(fragmentElement);
+ReactDOM.render(<MyFragment configuration={configuration} />, fragmentElement);
+Liferay.once('beforeNavigate', () => ReactDOM.unmountComponentAtNode(fragmentElement));
